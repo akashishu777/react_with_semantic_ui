fc97c01688ebf828519a9780e916d1625a025d31
Akash Vishwakarma <14cse031giet@gmail.com>
2018-08-21 19:15:58 +0530

    base

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import 'semantic-ui-css/semantic.min.css';
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Button } from 'semantic-ui-react'
+
+ReactDOM.render(<Button primary>Hello Sementic UI</Button>, document.getElementById('root'));

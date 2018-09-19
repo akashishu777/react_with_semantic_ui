4c0d33e70f326d1ea0ccceecdd4d19446588849e
Akash Vishwakarma <14cse031giet@gmail.com>
2018-09-19 21:13:12 +0530

    Adding drop down functionality

--- src/app.js
+++ src/app.js
@@ -2,22 +2,27 @@ import 'semantic-ui-css/semantic.min.css';
 
 import React from 'react'
 import { Dropdown } from 'semantic-ui-react'
+import { Divider, Form, Label } from 'semantic-ui-react'
+
 
 export class MyComponent extends React.Component{
 
     constructor(){
         super();
         this.state = {
-            data: [ { key: 'Game', value: 'Game', text: 'Game' },
-            { key: 'Place', value: 'Place', text: 'Place' },
-            { key: 'Month', value: 'Month', text: 'Month' }],
 
-            datachild:[  { value: 'Game', text: 'Game' },
-            {  value: 'Place', text: 'Place' },
-            { value: 'Month', text: 'Month' }],   
+            data: [ { key: 'Game', value: 'Game', text: 'Game' }, { key: 'Place', value: 'Place', text: 'Place' }, { key: 'Month', value: 'Month', text: 'Month' }],
+
+            category:{
+                "Game" : ["GTA","NINJA"],
+                "Month" : ["JAN","FEB"],
+                "Place" : ["DELHI","MUMBAI"]
+              },   
 
             pop:[]
         };
+
+        this.handleChange = this.handleChange.bind(this);
     }
 
     componentWillMount() {
@@ -29,15 +34,52 @@ export class MyComponent extends React.Component{
           value: [],
         })
       }
-    
-    handleChange (event,data){
-        console.log(data.value);
 
+      componentDidUpdate(prevProps, prevState) {
+          console.log(this.state.pop); 
+      }
+
+    handleChange (event,data){
+
+        var as = this.state.category[data.value];
+        var tempPop = [];
+        
+        for(var i=0 ; i< as.length; i++){
+            tempPop.push({key: as[i], value: as[i], text: as[i]});
+        }
+
+        this.setState({pop: tempPop});
+        
     }
 
     render() {
-       return <div style={{marginTop:'100px',marginLeft:'400px'}}>
-
+       return <div style={{padding:"200px"}}>
+                <Form>
+          <Form.Field>
+            <input type='text' placeholder='First name' />
+            <Label pointing>Please enter a value</Label>
+          </Form.Field>
+          <Divider />
+      
+          <Form.Field>
+            <Label pointing='below'>Please enter a value</Label>
+            <input type='text' placeholder='Last Name' />
+          </Form.Field>
+          <Divider />
+      
+          <Form.Field inline>
+            <input type='text' placeholder='Username' />
+            <Label pointing='left'>That name is taken!</Label>
+          </Form.Field>
+          <Divider />
+      
+          <Form.Field inline>
+            <Label pointing='right'>Your password must be 6 characters or more</Label>
+            <input type='password' placeholder='Password' />
+          </Form.Field>
+        </Form>
+        <Divider />
+        <Form.Field inline>
                 <Dropdown
                     options={this.state.data}
                     placeholder='Select'
@@ -45,10 +87,12 @@ export class MyComponent extends React.Component{
                     selection
                 />             
                     <Dropdown
-                    options={this.state.datachild}
+                    style={{marginLeft:"20px"}}
+                    options={this.state.pop}
                     placeholder='Select'
                     selection
-                />             
+                />     
+       </Form.Field>       
               </div>
        }
  }
